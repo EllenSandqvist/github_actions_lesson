65679803788469378ee37a7b18194c26230f462d
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-10-31 15:45:30 +0100

    changed code coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -14,9 +14,9 @@ const config: Config = {
   collectCoverage: true, // Aktivera kodtäckning
   coverageThreshold: {
     global: {
-      branches: 80, // Minst 100% branch-täckning
-      functions: 80, // Minst 100% funktionstäckning
-      lines: 80, // Minst 100% linjetäckning
+      branches: 40, // Minst 100% branch-täckning
+      functions: 40, // Minst 100% funktionstäckning
+      lines: 40, // Minst 100% linjetäckning
       statements: 40, // Minst 100% uttalande-täckning
     },
   },
