8f279463861798b8b71d4257fc30da066082c972
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-11-05 10:35:05 +0100

    changed test settings

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 });
 
 // Only start the server if we're not in a Vercel environment
-if (process.env.VERCEL !== '1') {
+if (process.env.VERCEL !== '1' && process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
